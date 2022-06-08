079340958e3c5bde38813cfa491c31ee6d00c414
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-06-07 22:47:15 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-
+import { FaGithub, FaLinkedin, FaEnvelope, FaMap, FaHome, FaRocket, FaSearch } from 'react-icons/fa'
 import CountryDataPage from "./pages/CountryDataPage"
 import ErrorPage from "./pages/ErrorPage";
 import MapPage from "./pages/MapPage";
 
-function App({ navIcons, search }) {
+function App() {
+  const iconLinks = [
+    {
+      id: 0, colorCode: ' #0077b5', iconName: FaHome,
+      name: 'Home', pathName: '/'
+    },
+    {
+      id: 1, colorCode: '8cac5', iconName: FaMap,
+      name: 'Map', pathName: '/Map'
+    },
 
+    {
+      id: 2, colorCode: '8cac5', iconName: FaRocket,
+      name: 'Portfolio', pathName: ''
+    },
+    // {
+    //   id: 3, colorCode: '8cac5', iconName: FaSearch,
+    //   name: 'search', pathName: ''
+    // },
+    {
+      id: 4, colorCode: 'black', iconName: FaGithub,
+      name: 'Github', pathName: '/https://github.com/dopoku12'
+    },
+    {
+      id: 5, colorCode: ' #0077b5', iconName: FaLinkedin,
+      name: 'Linkedin', pathName: '/https://www.linkedin.com/in/david-opoku-7008721b7'
+    },
+    {
+      id: 6, colorCode: '78cac5', iconName: FaEnvelope,
+      name: 'Email', pathName: '/'
+    }]
+  const navIcons = iconLinks.filter((i) => i.id <= 2)
 
-  const headerLinks = navIcons.map((i) =>
-    <li key={i.id} className={i.name} >
-      <Link to={i.pathName}>
-        <p>
-          <i.iconName size={25} color={i.colorCode} />
-          {i.name}
-        </p>
-      </Link>
-
-    </li >
-  )
-
-  console.log(window.location.href)
-
+  function searchHandler() {
+    console.log(window.location.href)
+  }
 
   return (
     <div className="App">
@@ -29,11 +48,22 @@ function App({ navIcons, search }) {
         <nav>
           <ul className="header-nav-ul">
             {
-              headerLinks
+              navIcons.map((i) =>
+                <li key={i.id} className={i.name} >
+                  <Link to={i.pathName}>
+                    <p>
+                      <i.iconName size={25} color={i.colorCode} />
+                      {i.name}
+                    </p>
+                  </Link>
+                </li>
+              )
             }
             <li className="input-group">
               <input type="search" id="search-box" placeholder="Enter County Name" />
-              <search className="search-icon" onClick={''} />
+              {
+                <FaSearch className="search-icon" onClick={searchHandler} />
+              }
             </li>
           </ul>
         </nav>
@@ -41,17 +71,11 @@ function App({ navIcons, search }) {
         <Routes>
           <Route path="/" element={<CountryDataPage />} />
           <Route path="/Map" element={<MapPage />} />
-          <Route path="/https://davidopoku-portfolio.netlify.app"
-            element={() => {
-              window.location.href = '/https://davidopoku-portfolio.netlify.app';
-              return null
-            }}
-          />
+          <Route path="/https://davidopoku-portfolio.netlify.app" />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
-
-
       </BrowserRouter>
+      <CountryDataPage footerIcons={iconLinks.filter((i) => i.id > 3)} />
     </div>
   );
 }
--- src/pages/CountryDataPage.js
+++ src/pages/CountryDataPage.js
@@ -2,37 +2,30 @@ import Flags from "../components/Flags"
 
 
 function CountryDataPage({ footerIcons }) {
-
-
-    const footerLinks = footerIcons.map((i) =>
-        <li key={i.id} className={i.name} >
-            <a target='_blank' rel="noreferrer" href={i.pathName} >
-                <p>
-                    <i.iconName size={25} color={i.colorCode} />
-                    {i.name}
-                </p>
-            </a>
-        </li>
-    )
-
+    console.log('footerIcons:', footerIcons);
     return (
         <div className="left-background">
             <main>
                 <Flags />
-
-
-
             </main>
 
             <footer>
                 <ul className='footer-nav-ul'>
                     {
-                        footerLinks
+                        footerIcons.map((i) =>
+                            <li key={i.id} className={i.name} >
+                                <a target='_blank' rel="noreferrer" href={i.pathName} >
+                                    <p>
+                                        <i.iconName size={25} color={i.colorCode} />
+                                        {i.name}
+                                    </p>
+                                </a>
+                            </li>
+                        )
                     }
                 </ul>
             </footer>
         </div>
     )
 }
-
 export default CountryDataPage
