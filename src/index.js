1362d3b927b7d5a4cd35510b16f1f7db8f253899
David Opoku <71300793+dopoku12@users.noreply.github.com>
2023-02-14 17:05:54 -0500

    new router update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import ErrorPage from './pages/ErrorPage';
 import './stylesheet/index.css';
 import App from './App';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+
 );
+
 
